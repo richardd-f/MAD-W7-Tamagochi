67208b187ed543636b3288dd288763bc39fc337e
Felix <ffelixrichardo@gmail.com>
2026-04-09 07:46:54 +0700

    Initial Commit

---
+++ Tamagochi/TamagochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagochiApp.swift
+//  Tamagochi
+//
+//  Created by student on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TamagochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
